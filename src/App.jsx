26f7affab17c42744889cc938bf3d3e68af18220
Voote <kszygy@gmail.com>
2023-03-12 14:29:02 +0000

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import Landing from "./organisms/Landing";
+
+const Root = () => (
+  <div>
+    <Landing />
+  </div>
+);
+
+export default Root;
---
+++ src/organisms/Landing.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import PostsBox from "../molecules/PostsBox";
+
+const Landing = () => (
+  <div>
+    <PostsBox />
+  </div>
+);
+
+export default Landing;
